29b5aebe8a180d2356ad77a55c2715f30dcbab2a
Tiwit27 <lojekrafal27@gmail.com>
2024-12-04 18:36:42 +0100

    Kalkulator

---
+++ Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kalkulator
+{
+    public partial class MainWindow : Window
+    {
+        decimal[] memory = new decimal[2];
+        Sign memorySign = Sign.nothing;
+        bool isResult = false;
+        bool clear = false;
+        enum Sign
+        {
+            nothing,
+            plus,
+            minus,
+            multiplication,
+            division
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(isResult)
+            {
+                textVisual.Text = "0";
+            }
+            if(textVisual.Text.Length < 9 || clear)
+            {
+                Button button = (Button)sender;
+                if(textVisual.Text == "0" || clear)
+                {
+                    textVisual.Text = "";
+                    clear = false;
+                }
+                textVisual.AppendText(button.Content.ToString());
+            }
+            isResult = false;
+        }
+        private void MakeCalkulation(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            char Sign = button.Content.ToString()[0];
+            if (textVisual.Text[textVisual.Text.Length - 1] == ',')
+            {
+                textVisual.Text = textVisual.Text.Remove(textVisual.Text.Length - 1);
+            }
+            if (!String.IsNullOrEmpty(textVisual.Text))
+            {
+                memory[0] = decimal.Parse(textVisual.Text);
+            }
+            switch (Sign)
+            {
+                case '+':
+                    if(!String.IsNullOrEmpty(textVisual.Text))
+                    {
+                        memorySign = MainWindow.Sign.plus;
+                    }
+                    break;
+                case '-':
+                    if (!String.IsNullOrEmpty(textVisual.Text))
+                    {
+                        memorySign = MainWindow.Sign.minus;
+                    }
+                    break;
+                case 'x':
+                    if (!String.IsNullOrEmpty(textVisual.Text))
+                    {
+                        memorySign = MainWindow.Sign.multiplication;
+                    }
+                    break;
+                case '÷':
+                    if (!String.IsNullOrEmpty(textVisual.Text))
+                    {
+                        memorySign = MainWindow.Sign.division;
+                    }
+                    break;
+            }
+            clear = true;
+        }
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            if (textVisual.Text[textVisual.Text.Length - 1] == ',')
+            {
+                textVisual.Text = textVisual.Text.Remove(textVisual.Text.Length - 1);
+            }
+            if (!String.IsNullOrEmpty(textVisual.Text))
+            {
+                memory[1] = decimal.Parse(textVisual.Text);
+                textVisual.Text = "0";
+            }
+            switch(memorySign)
+            {
+                case Sign.plus:
+                    textVisual.Text = (memory[0] + memory[1]).ToString();
+                    break;
+                case Sign.minus:
+                    textVisual.Text = (memory[0] - memory[1]).ToString();
+                    break;
+                case Sign.multiplication:
+                    textVisual.Text = (memory[0] * memory[1]).ToString();
+                    break;
+                case Sign.division:
+                    if (memory[1] != 0)
+                    {
+                        textVisual.Text = (memory[0] / memory[1]).ToString();
+                    }
+                    else
+                    {
+                        textVisual.Text = "ERROR";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            if(textVisual.Text.Length > 11)
+            {
+                textVisual.Text = "TOO BIG";
+            }
+            memory[0] = 0;
+            memory[1] = 0;
+            isResult = true;
+            memorySign = Sign.nothing;
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            memory[0] = 0;
+            memory[1] = 0;
+            isResult = false;
+            textVisual.Text = "0";
+        }
+        private void DelLast(object sender, RoutedEventArgs e)
+        {
+            if(textVisual.Text.Length > 1)
+            {
+                textVisual.Text = textVisual.Text.Remove(textVisual.Text.Length - 1);
+            }
+            else if(textVisual.Text.Length == 1 && textVisual.Text[0] != '0')
+            {
+                textVisual.Text = "0";
+            }
+        }
+        private void Dot(object sender, RoutedEventArgs e)
+        {
+            if (isResult)
+            {
+                textVisual.Text = "0";
+            }
+            if(!textVisual.Text.Contains(','))
+            {
+                textVisual.AppendText(",");
+            }
+            isResult = false;
+            clear = false;
+        }
+    }
+}
