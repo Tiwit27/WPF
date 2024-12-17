a0647207ff3a05635073b3217fc9b99d326704fe
Shautek <zs6sobieski.edu.pl>
2024-12-17 14:00:42 +0100

    ToDoList

---
+++ ToDoList/DB.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows.Controls;
+
+namespace ToDoList
+{
+    internal class DB
+    {
+        static MySqlConnection conn;
+        const string connectionData = "server=localhost;uid=root;pwd=;database=todolist";
+        public static bool Connect()
+        {
+            try
+            {
+                conn = new MySqlConnection(connectionData);
+                conn.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static void Command(StackPanel listStackPanel)
+        {
+            MySqlCommand myCommand = new MySqlCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = "SELECT * from list";
+            var myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                TextBlock textbox = new();
+                textbox.Text = myReader.GetValue(1).ToString();
+                listStackPanel.Children.Add(textbox);
+            }
+            myReader.Close();
+        }
+        public static void Add(TextBox textBox, StackPanel listStackPanel)
+        {
+            MySqlCommand myCommand = new MySqlCommand();
+            myCommand.Connection = conn;
+            myCommand.CommandText = $"INSERT INTO list(title) VALUES ('{textBox.Text}')";
+            myCommand.ExecuteNonQuery();
+            textBox.Clear();
+            Reload(listStackPanel);
+        }
+        public static void Reload(StackPanel listStackPanel)
+        {
+            listStackPanel.Children.Clear();
+            Command(listStackPanel);
+        }
+    }
+}
---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace ToDoList
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            DB.Connect();
+            DB.Command(listStackPanel);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(input.Text))
+            {
+                DB.Add(input, listStackPanel);
+            }
+        }
+    }
+}
